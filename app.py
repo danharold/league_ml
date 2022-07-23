5af646c23c55479ab18bfb79fbf2cfbe1e2925b1
Dan Harold <dan.harold2001@hotmail.com>
2022-07-23 20:44:12 +0100

    miau

--- app.py
+++ app.py
@@ -3,11 +3,11 @@ import os
 
 import aws_cdk as cdk
 
-from league_ml.league_ml_stack import LeagueMlStack
+from league_ml.league_ml_stack import LeagueMLStack
 
 
 app = cdk.App()
-LeagueMlStack(app, "LeagueMlStack",
+LeagueMLStack(app, "LeagueMLStack",
     # If you don't specify 'env', this stack will be environment-agnostic.
     # Account/Region-dependent features and context lookups will not work,
     # but a single synthesized template can be deployed anywhere.
