b459df2ad7690df5f350b0e2bfc7d6d1ee0974e4
Dan Harold <dan.harold2001@hotmail.com>
2022-07-24 14:53:00 +0100

    x

--- data_init/update_puuids.py
+++ data_init/update_puuids.py
@@ -7,55 +7,66 @@ import threading
 
 import boto3
 
+cf_client = boto3.client('cloudformation')
 ddb = boto3.client('dynamodb')
 
-f = open('puuids.json')
-puuids = np.array(json.load(f)['data'])
+# open file and split for multithreading
+f = open('data_init/puuids.json')
+puuids = np.array(json.load(f))
 x = np.array_split(puuids, 100)
 
-table_name = 'LeagueMLStack-Puuids82D0B762-17KCZA7BLHG1M'
+# access stack with cf to obtain table name from outputs
+stack_name = 'LeagueMLStack'
+response = cf_client.describe_stacks(StackName = stack_name)
+outputs = response['Stacks'][0]['Outputs']
+table_name = ''
+for output in outputs:
+    keyName = output['OutputKey']
+    if keyName == "DataTableName":
+        table_name = output['OutputValue']
+    
 
-# class AtomicInteger():
-#     def __init__(self, value=0):
-#         self._value = int(value)
-#         self._lock = threading.Lock()
+class AtomicInteger():
+    def __init__(self, value=0):
+        self._value = int(value)
+        self._lock = threading.Lock()
         
-#     def inc(self, d=1):
-#         with self._lock:
-#             self._value += int(d)
-#             return self._value
+    def inc(self, d=1):
+        with self._lock:
+            self._value += int(d)
+            return self._value
 
-#     def dec(self, d=1):
-#         return self.inc(-d)    
+    def dec(self, d=1):
+        return self.inc(-d)    
 
-#     @property
-#     def value(self):
-#         with self._lock:
-#             return self._value
+    @property
+    def value(self):
+        with self._lock:
+            return self._value
 
-#     @value.setter
-#     def value(self, v):
-#         with self._lock:
-#             self._value = int(v)
-#             return self._value
+    @value.setter
+    def value(self, v):
+        with self._lock:
+            self._value = int(v)
+            return self._value
 
-# count = AtomicInteger()
+count = AtomicInteger()
 
-# def update(p, count):
-#     for item in p:
-#         n = count.inc()
-#         if n % 100 == 0:
-#             print(n)
-#         k = "puuid/" + item
-#         ddb.put_item(TableName = table_name, Item={'key':{'S':k}})
+def update(p, count):
+    for item in p:
+        n = count.inc()
+        if n % 100 == 0:
+            print(n)
+        k = "puuid/" + item
+        ddb.put_item(TableName = table_name, Item={'key':{'S': k}})
 
-# threads = []
-# for i in range(100):   
-#     t = threading.Thread(target = update, args = (x[i], count))
-#     t.start()
-#     threads.append(t)
+threads = []
+for i in range(100):   
+    t = threading.Thread(target = update, args = (x[i], count))
+    t.start()
+    threads.append(t)
 
-# for i in range(100):
-#     threads[i].join()
+for i in range(100):
+    threads[i].join()
 
 
--- league_ml/league_ml_stack.py
+++ league_ml/league_ml_stack.py
@@ -9,45 +9,30 @@ from constructs import Construct
 
 class LeagueMLStack(Stack):
 
-    # @property
-    # def puuids_table_name(self):
-    #     return self._puuids_table_name
-    
-    # @property
-    # def match_table_name(self):
-    #     return self._puuids_table_name
+    @property
+    def table_name(self):
+        return self._table_name
 
 
     def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
         super().__init__(scope, construct_id, **kwargs)
 
         my_lambda = _lambda.Function(
-            self, 'UpdatePuuids',
+            self, 'CollectMatchIds',
             runtime=_lambda.Runtime.PYTHON_3_9,
             code=_lambda.Code.from_asset('lambda'),
-            handler='update_puuids'
+            handler='collect_match_ids'
         )
 
-        puuids_table = ddb.Table(
-            self, 'Puuids',
-            partition_key={'name' : 'puuid', 'type': ddb.AttributeType.STRING},
+        table = ddb.Table(
+            self, 'Data',
+            partition_key={'name' : 'key', 'type': ddb.AttributeType.STRING},
             billing_mode = ddb.BillingMode.PAY_PER_REQUEST,
             
         )
 
-        match_table = ddb.Table(
-            self, 'MatchData',
-            partition_key = {'name': 'match_id', 'type': ddb.AttributeType.STRING},
-            billing_mode=ddb.BillingMode.PAY_PER_REQUEST
+        self._table_name = CfnOutput(
+            self, 'DataTableName',
+            value=table.table_name
         )
-
-        # self._puuids_table_name = CfnOutput(
-        #     self, 'PuuidsTableName',
-        #     value=puuids_table.table_name
-        # )
-
-        # self._match_table_name = CfnOutput(
-        #     self, 'PuuidsTableName',
-        #     value=match_table.table_name
-        # )
         
